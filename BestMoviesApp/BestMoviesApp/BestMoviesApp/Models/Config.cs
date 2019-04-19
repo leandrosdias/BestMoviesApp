0b3b6cb6126518f299328d82adb35fe7081422d8
Leandro Dias <ldias@processwaregroup.com>
2019-04-19 17:32:49 -0300

    Create Config.cs
    Added model config.cs for store language chosen

---
+++ BestMoviesApp/BestMoviesApp/BestMoviesApp/Models/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestMoviesApp.Models
+{
+    class Config
+    {
+        public string Language { get; set; }    
+    }
+}
