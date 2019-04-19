19b1d6a9d5ff2a578c2c8cb7cd1a92d60f484383
Leandro Dias <ldias@processwaregroup.com>
2019-04-19 17:28:03 -0300

    Create IModelAccessor.cs
    Created interface for access model in any database

---
+++ BestMoviesApp/BestMoviesApp/BestMoviesApp/Database/IModelAccessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestMoviesApp.Database
+{
+    interface IModelAccessor
+    {
+        void Insert(object model);
+        void Update(object model);
+        void Delete(object model);
+        List<T> GetAll<T>() where T : new();
+    }
+}
