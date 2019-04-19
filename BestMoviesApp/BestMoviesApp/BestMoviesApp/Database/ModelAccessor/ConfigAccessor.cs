2caf3157e4b0462ad358b2d01d759c182d538bcd
Leandro Dias <ldias@processwaregroup.com>
2019-04-19 17:35:06 -0300

    Create Config Acessor
    Create and implement access to the Config model database

---
+++ BestMoviesApp/BestMoviesApp/BestMoviesApp/Database/ModelAccessor/ConfigAccessor.cs
@@ -0,0 +1,73 @@
+﻿using BestMoviesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestMoviesApp.Database.ModelAcessor
+{
+    class ConfigAccessor
+    {
+        private readonly IModelAccessor _modelAcessor;
+        private readonly Config _config;
+
+        public ConfigAccessor(IModelAccessor modelAcessor, Config config)
+        {
+            _modelAcessor = modelAcessor;
+            _config = config;
+        }
+
+        public void Insert()
+        {
+            _modelAcessor.Insert(_config);
+        }
+
+        public void Insert(Config config)
+        {
+            _modelAcessor.Insert(config);
+        }
+
+        public void Delete()
+        {
+            _modelAcessor.Delete(_config);
+        }
+
+        public void Update()
+        {
+            _modelAcessor.Update(_config);
+        }
+
+        public void Update(Config config)
+        {
+            _modelAcessor.Update(config);
+        }
+
+        public List<Config> GetAll()
+        {
+            return _modelAcessor.GetAll<Config>();
+        }
+
+        public void InsertOrUpdate()
+        {
+            var configList = GetAll();
+            if (configList.Count <= 0)
+                Insert();
+            else
+                Update();
+        }
+
+        public void InsertOrUpdate(Config config)
+        {
+            var configList = GetAll();
+            if (configList.Count <= 0)
+                Insert(config);
+            else
+                Update(config);
+        }
+
+        public Config GetConfig()
+        {
+            var configs = GetAll();
+            return configs.Count > 0 ? configs[0] : null;
+        }
+    }
+}
---
+++ BestMoviesApp/BestMoviesApp/BestMoviesApp/Helpers/ConfigHelper.cs
@@ -0,0 +1,34 @@
+﻿using BestMoviesApp.Database;
+using BestMoviesApp.Database.ModelAcessor;
+using BestMoviesApp.Models;
+using Plugin.Multilingual;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestMoviesApp.Helpers
+{
+    class ConfigHelper
+    {
+        public static void InsertConfig(Config config)
+        {
+            var modelAcessor = new SqlDataAccessor();
+            var configAcessor = new ConfigAccessor(modelAcessor, config);
+            configAcessor.Insert();
+        }
+
+        public static Config GetConfig()
+        {
+            var modelAcessor = new SqlDataAccessor();
+            var configAcessor = new ConfigAccessor(modelAcessor, new Config());
+            var config = configAcessor.GetConfig();
+            if (config == null)
+            {
+                config = new Config { Language = CrossMultilingual.Current.DeviceCultureInfo.IetfLanguageTag };
+                configAcessor.InsertOrUpdate(config);
+            }
+
+            return config;
+        }
+    }
+}
