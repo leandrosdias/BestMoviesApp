7f3eec6f717faccb58c3d1e9a742534ecd343122
Leandro Dias <ldias@processwaregroup.com>
2019-04-20 09:52:01 -0300

    Create MessageService
    Create interface to show messages

---
+++ BestMoviesApp/BestMoviesApp/BestMoviesApp/Interfaces/IMessageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestMoviesApp.Interfaces
+{
+    internal interface IMessageService
+    {
+        Task ShowAsync(string message);
+        Task<bool> ShowAnswerAsync(string message, string ok = "OK", string cancel = "CANCEL");
+
+    }
+}
