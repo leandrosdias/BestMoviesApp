95260b70f4e0041b670f28ce4dce02b95695618a
Leandro Dias <ldias@processwaregroup.com>
2019-04-20 15:00:35 -0300

    Update Config.cs
    Adjust class config

--- BestMoviesApp/BestMoviesApp/BestMoviesApp/Models/Config.cs
+++ BestMoviesApp/BestMoviesApp/BestMoviesApp/Models/Config.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,8 @@ namespace BestMoviesApp.Models
 {
     class Config
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
         public string Language { get; set; }    
     }
 }
