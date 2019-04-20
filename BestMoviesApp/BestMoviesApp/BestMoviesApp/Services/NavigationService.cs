9f1b1502acdde4c1b63ff97b19a7833fb5121b8a
Leandro Dias <ldias@processwaregroup.com>
2019-04-20 11:28:29 -0300

    Update NavigationService.cs
    Create navigation for pages

--- BestMoviesApp/BestMoviesApp/BestMoviesApp/Services/NavigationService.cs
+++ BestMoviesApp/BestMoviesApp/BestMoviesApp/Services/NavigationService.cs
@@ -1,5 +1,6 @@
 ﻿using BestMoviesApp.Interfaces;
 using BestMoviesApp.Utils;
+using BestMoviesApp.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,10 +10,26 @@ namespace BestMoviesApp.Services
 {
     internal class NavigationService : INavigationService
     {
-        public Task NavigateToPageChoiced(ItemChoice itemChoice)
+        public async Task NavigateToPageChoiced(ItemChoice itemChoice)
         {
             switch (itemChoice)
             {
+                case ItemChoice.UpcomingMovies:
+                    await Xamarin.Forms.Application.Current.MainPage.Navigation.PushAsync(new UpcomingMoviesPage());
+                    break;
+
+                case ItemChoice.TopRatedMovies:
+                    await Xamarin.Forms.Application.Current.MainPage.Navigation.PushAsync(new TopRatedPage());
+                    break;
+
+                case ItemChoice.Settings:
+                    await Xamarin.Forms.Application.Current.MainPage.Navigation.PushAsync(new SettingsPage());
+                    break;
+
+                case ItemChoice.FavoritesMovies:
+                    await Xamarin.Forms.Application.Current.MainPage.Navigation.PushAsync(new FavoritesPage());
+                    break;
+
 
                 default:
                     throw new NotImplementedException();
