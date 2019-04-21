557a7b7a0e96bfde0cc568223210b8a2ceeb3ecb
Leandro Dias <ldias@processwaregroup.com>
2019-04-21 01:02:50 -0300

    Implement UpcomingMovies
    Implementation logic for show movies from the API

--- BestMoviesApp/BestMoviesApp/BestMoviesApp/ViewModels/UpcomingMoviesViewModel.cs
+++ BestMoviesApp/BestMoviesApp/BestMoviesApp/ViewModels/UpcomingMoviesViewModel.cs
@@ -1,10 +1,49 @@
-﻿using System;
+﻿using BestMoviesApp.Helpers;
+using BestMoviesApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BestMoviesApp.ViewModels
 {
     class UpcomingMoviesViewModel : BaseViewModel
     {
+        ObservableCollection<MovieLineGrid> moviesGrid;
+        public ObservableCollection<MovieLineGrid> MoviesGrid
+        {
+            get => moviesGrid;
+            set { moviesGrid = value; Notify("MoviesGrid"); }
+        }
+
+        private int _page;
+        public int Page
+        {
+            get => _page;
+            set { _page = value; Notify("Page"); }
+        }
+
+        public UpcomingMoviesViewModel(List<Movie> movies)
+        {
+            MoviesGrid = new ObservableCollection<MovieLineGrid>();
+            MoviesGrid.CollectionChanged += Movies_CollectionChanged;
+            Page = 1;
+            MovieLineGridHelper.AddMoviesInScreen(MoviesGrid, movies);
+        }
+
+        private void Movies_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            Notify("MoviesGrid");
+        }
+
+        public async Task GetNextPageAsync()
+        {
+            Page++;
+            var movies = await MovieHelper.GetUpcommingMoviesAsync(_page);
+            MovieLineGridHelper.AddMoviesInScreen(MoviesGrid, movies);
+        }
     }
 }
--- BestMoviesApp/BestMoviesApp/BestMoviesApp/Views/UpcomingMoviesPage.xaml.cs
+++ BestMoviesApp/BestMoviesApp/BestMoviesApp/Views/UpcomingMoviesPage.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using BestMoviesApp.ViewModels;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,10 +9,21 @@ namespace BestMoviesApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UpcomingMoviesPage : ContentPage
     {
-        public UpcomingMoviesPage()
+        public UpcomingMoviesPage(List<Movie> movies)
         {
             InitializeComponent();
-            BindingContext = new UpcomingMoviesViewModel();
+            BindingContext = new UpcomingMoviesViewModel(movies);
+
+        }
+
+        private void ListView_ItemAppearing(object sender, ItemVisibilityEventArgs e)
+        {
+            var nItems = int.Parse(Page.Text) * 20 / 6;
+            if (nItems == e.ItemIndex)
+            {
+                var viewModel = (UpcomingMoviesViewModel)BindingContext;
+                viewModel.GetNextPageAsync();
+            }
         }
     }
 }
