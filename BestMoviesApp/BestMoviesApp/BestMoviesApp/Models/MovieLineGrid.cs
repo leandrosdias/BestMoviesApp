5f134ec1eae413386f0f12d9a8e5711311dc5b3a
Leandro Dias <ldias@processwaregroup.com>
2019-04-21 20:17:26 -0300

    Update MovieLineGrid.cs
    Add id in listview

--- BestMoviesApp/BestMoviesApp/BestMoviesApp/Models/MovieLineGrid.cs
+++ BestMoviesApp/BestMoviesApp/BestMoviesApp/Models/MovieLineGrid.cs
@@ -6,11 +6,13 @@ namespace BestMoviesApp.Models
 {
     class MovieLineGrid
     {
-        public string Id { get; set; }
+        public int Id1 { get; set; }
         public string Image1 { get; set; }
         public string Title1 { get; set; }
+        public int Id2 { get; set; }
         public string Image2 { get; set; }
         public string Title2 { get; set; }
+        public int Id3 { get; set; }
         public string Image3 { get; set; }
         public string Title3 { get; set; }
     }
