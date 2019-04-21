c01904fd391d51e6677bede723cda91fd46eca29
Leandro Dias <ldias@processwaregroup.com>
2019-04-21 01:01:16 -0300

    Create MovieLineGrid.cs
    Create class for manage lines of grid in UpcommingMoviesClass

---
+++ BestMoviesApp/BestMoviesApp/BestMoviesApp/Models/MovieLineGrid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestMoviesApp.Models
+{
+    class MovieLineGrid
+    {
+        public string Id { get; set; }
+        public string Image1 { get; set; }
+        public string Title1 { get; set; }
+        public string Image2 { get; set; }
+        public string Title2 { get; set; }
+        public string Image3 { get; set; }
+        public string Title3 { get; set; }
+    }
+}
