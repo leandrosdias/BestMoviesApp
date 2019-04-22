20002c2812e73f3d2c61efab900d212de3f3ac85
Leandro Dias <ldias@processwaregroup.com>
2019-04-21 21:42:23 -0300

    Update Movie.cs

--- BestMoviesApp/BestMoviesApp/BestMoviesApp/Models/Movie.cs
+++ BestMoviesApp/BestMoviesApp/BestMoviesApp/Models/Movie.cs
@@ -4,6 +4,7 @@ using SQLite;
 public class Movie
 {
     [JsonProperty("vote_count")]
+    [Ignore]
     public int VoteCount { get; set; }
 
     [JsonProperty("id")]
@@ -11,33 +12,40 @@ public class Movie
     public int Id { get; set; }
 
     [JsonProperty("video")]
+    [Ignore]
     public bool Video { get; set; }
 
     [JsonProperty("vote_average")]
+    [Ignore]
     public float VoteAverage { get; set; }
 
     [JsonProperty("title")]
     public string Title { get; set; }
 
     [JsonProperty("popularity")]
+    [Ignore]
     public float Popularity { get; set; }
 
     [JsonProperty("poster_path")]
     public string PosterPath { get; set; }
 
     [JsonProperty("original_language")]
+    [Ignore]
     public string OriginalLanguage { get; set; }
 
     [JsonProperty("original_title")]
+    [Ignore]
     public string OriginalTitle { get; set; }
 
     [JsonProperty("genre_ids")]
+    [Ignore]
     public int[] GenreIds { get; set; }
 
     [JsonProperty("backdrop_path")]
     public string BackdropPath { get; set; }
 
     [JsonProperty("adult")]
+    [Ignore]
     public bool Adult { get; set; }
 
     [JsonProperty("overview")]
@@ -45,4 +53,7 @@ public class Movie
 
     [JsonProperty("release_date")]
     public string ReleaseDate { get; set; }
+
+    [JsonIgnore]
+    public bool IsFavorite { get; set; }
 }
